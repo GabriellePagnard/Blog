3172248ff02a09542dd2c1fb9df941bd25eb9fd4
Gabrielle Pagnard <gpagnard@gmail.com>
2024-12-23 17:18:30 +0100

    Ajout de la liste des articles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const articles = [
+  { id: 1, title: 'Premier article', summary: 'Résumé du premier article' },
+  { id: 2, title: 'Second article', summary: 'Résumé du second article' },
+];
 
 const Home = () => (
   <div>
     <h1>Accueil</h1>
-    <p>Bienvenue sur le blog !</p>
+    <ul>
+      {articles.map((article) => (
+        <li key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.summary}</p>
+          <Link to={`/article/${article.id}`}>Lire la suite</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
